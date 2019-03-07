c264912d23c3420ff47fe099957e554f31266494
Antony LEFEVRE <lefevreanto57@gmail.com>
2019-03-07 11:20:02 +0100

    commit from VS

---
+++ FicheDePayeJSON/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace FicheDePayeJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Consigne
+            /* -garder l'hsitorique sans multiplication de données
+             * ecrire un prog qui permet de gérer les fiche de paie => ajout / suppression / modifier
+             * faire janvier / février 
+             * afficher l'historique des modif */
+            #endregion
+
+            
+            Salarié Antony = new Salarié("LEFEVRE", "Antony", new DateTime(1993, 10, 07));
+            Contrat ContratAntony = new Contrat(Antony, 10);
+
+
+            Console.WriteLine("date de début de période");
+            string coucou = Console.ReadLine();
+            DateTime DebutPeriode =  DateTime.Parse(coucou);
+            Console.WriteLine("date de fin de période");
+            coucou = Console.ReadLine();
+            DateTime FinPeriode =  DateTime.Parse(coucou);
+            TimeSpan HeuresTotal = FinPeriode.Subtract(DebutPeriode);
+            Console.WriteLine(DebutPeriode);
+            Console.WriteLine(FinPeriode);
+
+            
+            FicheDePaye FDPAntony = new FicheDePaye(Antony, ContratAntony, HeuresTotal.TotalHours ,vacances(), DebutPeriode.ToLongDateString(), FinPeriode.ToLongDateString());
+            Console.WriteLine(FDPAntony);
+
+        }
+
+        static public double vacances()
+        {
+            string userChoice;
+            double JourDeVacances = 0.0;
+            Console.WriteLine("Y a-t-il eu des vacances pour ce mois ?");
+            userChoice = Console.ReadLine();
+            if (userChoice == "oui")
+            {
+                Console.WriteLine("Date de début des vacances format dd/MM/YYYY");
+                string DateDebutVac = Console.ReadLine();
+                DateTime DateDebutVacances = DateTime.Parse(DateDebutVac);
+                Console.WriteLine("Date de fin des vacances format dd/MM/YYYY");
+                string DateFinVac = Console.ReadLine();
+                DateTime DateFinVacances = DateTime.Parse(DateFinVac);
+                JourDeVacances = DateFinVacances.Subtract(DateDebutVacances).TotalDays;
+            }
+            return JourDeVacances;
+        }
+    }
+}
---
+++ FicheDePayeJSON/Salarié.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FicheDePayeJSON
+{
+    class Salarié
+    {
+        private string _nom;
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        private string _prenom;
+        public string Prenom
+        {
+            get { return _prenom; }
+            set { _prenom = value; }
+        }
+
+        private DateTime _dateDeNaissance;
+        public DateTime DateDeNaissance
+        {
+            get { return _dateDeNaissance; }
+            set { _dateDeNaissance = value; }
+        }
+
+
+        public Salarié(string nom, string prenom, DateTime dateDeNaissance)
+        {
+            _nom = nom;
+            _prenom = prenom;
+            _dateDeNaissance = dateDeNaissance;
+        }
+    }
+}
